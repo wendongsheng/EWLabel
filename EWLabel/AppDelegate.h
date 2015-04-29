59fd05e1b0aca2f637dc673e54236fe717ee8e09
wendongsheng <18637117435@163.com>
2015-04-29 10:10:19 +0800

    Initial Commit

---
+++ EWLabel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EWLabel
+//
+//  Created by wendongsheng on 15/4/29.
+//  Copyright (c) 2015年 etiantian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EWLabel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EWLabel
+//
+//  Created by wendongsheng on 15/4/29.
+//  Copyright (c) 2015年 etiantian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
